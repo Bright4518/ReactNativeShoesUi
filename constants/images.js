0913bba42e76d11172a6094c7b5856c32365e76c
Claude-Bright git <claude4518@gmail..com>
2020-11-08 23:37:30 +0100

    my First commit

---
+++ constants/images.js
@@ -0,0 +1,30 @@
+
+export const nikeMecon3 = require("../assets/images/nike-metcon-3.png");
+export const nikeMecon4 = require("../assets/images/nike-metcon-4.png");
+export const nikeMecon58Black = require("../assets/images/nike-metcon-5-black.png");
+
+export const nikeMecon5Purple = require("../assets/images/nike-metcon-5-purple.png");
+
+export const nikeMecon6 = require("../assets/images/nike-metcon-6.png");
+
+export const nikeMeconFree = require("../assets/images/nike-metcon-free.png");
+
+export const nikePegasu36 = require("../assets/images/nike-pegasus-36.png");
+
+export const nikeZoomKobe1Proto = require("../assets/images/nike-zoom-kobe-1-protro.png");
+
+export const recentlyViewedLabel = require("../assets/images/recently-viewed-label.png");
+
+
+
+export default {
+    nikeMecon3,
+    nikeMecon4,
+    nikeMecon58Black,
+    nikeMecon5Purple,
+    nikeMecon6,
+    nikeMeconFree,
+    nikePegasu36,
+    nikeZoomKobe1Proto,
+    recentlyViewedLabel
+}
